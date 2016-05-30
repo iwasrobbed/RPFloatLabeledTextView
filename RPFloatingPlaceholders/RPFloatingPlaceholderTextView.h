f9bfd5be78cd69dcb16ef52321963078ee807065
klauslanza <klauslanza@gmail.com>
2016-05-30 13:37:56 +0200

    Added new property.

--- RPFloatingPlaceholders/RPFloatingPlaceholderTextView.h
+++ RPFloatingPlaceholders/RPFloatingPlaceholderTextView.h
@@ -1,5 +1,5 @@
 //
-//  RPFloatingPlaceholderTextView.h
+//  RPFloatingPlaceholderTextField.h
 //  RPFloatingPlaceholders
 //
 //  Created by Rob Phillips on 10/19/13.
@@ -11,7 +11,7 @@
 #import <UIKit/UIKit.h>
 #import "RPFloatingPlaceholderConstants.h"
 
-@interface RPFloatingPlaceholderTextView : UITextView
+@interface RPFloatingPlaceholderTextField : UITextField
 
 /**
  Enum to switch between upward and downward animation of the floating label.
@@ -19,18 +19,13 @@
 @property (nonatomic) RPFloatingPlaceholderAnimationOptions animationDirection;
 
 /**
- The string that is displayed when there is no other text in the text view.
- */
-@property (nonatomic, copy) NSString *placeholder;
-
-/**
- The floating label that is displayed above the text view when there is other 
- text in the text view.
+ The floating label that is displayed above the text field when there is other
+ text in the text field.
  */
 @property (nonatomic, strong, readonly) UILabel *floatingLabel;
 
 /**
- The color of the floating label displayed above the text view when it is in
+ The color of the floating label displayed above the text field when it is in
  an active state (i.e. the associated text view is first responder).
  
  @discussion Note: Tint color is used by default if this is nil.
@@ -38,12 +33,17 @@
 @property (nonatomic, strong) UIColor *floatingLabelActiveTextColor;
 
 /**
- The color of the floating label displayed above the text view when it is in
+ The color of the floating label displayed above the text field when it is in
  an inactive state (i.e. the associated text view is not first responder).
  
  @discussion Note: 70% gray is used by default if this is nil.
  */
 @property (nonatomic, strong) UIColor *floatingLabelInactiveTextColor;
+
+/**
+  The font of the floating label displayed above the text field.
+ */
+@property (nonatomic, strong) UIFont *floatingLabelFont;
 
 /**
  The default color of the text field's placeholder text
